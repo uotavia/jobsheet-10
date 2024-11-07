c5658103f5580dd62daba2fed23667971bf7a6b5
uotavia <uotavia@gmail.com>
2024-11-07 13:58:41 +0700

    first commit

---
+++ Bioskop20.java
@@ -0,0 +1,29 @@
+public class Bioskop20 {
+    public static void main(String[] args) {
+        String[][] penonton = new String[4][2];
+
+        penonton [0][0] = "Amin";
+        penonton [0][1] = "Bena";
+        penonton [1][0] = "Candra";
+        penonton [1][1] = "Dela";
+        penonton [2][0] = "Eka";
+        penonton [2][1] = "Farhan";
+        penonton [3][0] = "Gisel";
+        penonton [3][1] = "Gisel";
+
+        for(int i = 0; i<penonton.length; i++){
+            System.out.println("Penonton pada baris ke-"+(i+1)+": "+ String.join(",", penonton[i]));
+        }
+
+        System.out.println(penonton.length);
+        
+        for(String [] barisPenonton : penonton){
+            System.out.println("Panjang baris: "+ barisPenonton.length);
+        }
+        System.out.println("Penonton pada baris ke-3: ");
+        for(String i : penonton[2]){
+            System.out.println(i );
+        }
+
+    }
+}
